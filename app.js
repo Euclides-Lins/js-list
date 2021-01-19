9b6f8c0105aa4172b9268017f2e702af0cbaebe3
Euclides-Lins <euclidesvasconcelos01@gmail.com>
2021-01-18 20:20:29 -0400

    chore: express added and router initialized

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const teste = 222;
-console.log(teste);
+import express from 'express';
+import home from './src/routes/home';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', home);
+  }
+}
+
+export default new App().app;
