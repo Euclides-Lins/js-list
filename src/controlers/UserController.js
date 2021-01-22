cf6e4da1ef91cdad9e8c5e431c20a0dcd40d11ba
Euclides-Lins <euclidesvasconcelos01@gmail.com>
2021-01-22 17:23:33 -0400

    feat: DELETE users feature added

--- src/controlers/UserController.js
+++ src/controlers/UserController.js
@@ -50,6 +50,29 @@ class HomeController {
       return res.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
   }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID NÃO ENVIADO'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+      return res.json('Usuário não existe mais');
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
 }
 
 export default new HomeController();
